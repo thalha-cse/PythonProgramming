ac5932241f24723d0b2c20c006340642ab7b4f6c
MOHAMMED THALHA J <96979827+Mohammed-Thalha@users.noreply.github.com>
2022-01-09 18:59:07 +0530

    Create 05. Emojis.py

---
+++ Projects/05. Emojis.py
@@ -0,0 +1,9 @@
+# This project is for fun, if you enter the following commands it will give you the emojis
+
+print("-------------")
+print("\U0001F917")
+print("\U0001F637")
+print("\U0001F62A")
+print("\U0001F618")
+print("\U0001F600")
+print("-------------")

76a6a964112f43c5be0424087b42870d288061ac
MOHAMMED THALHA J <96979827+Mohammed-Thalha@users.noreply.github.com>
2022-01-23 07:29:13 -0800

    Create 14. Detect Number.py

---
+++ Basic Python Programs/14. Detect Number.py
@@ -0,0 +1,8 @@
+num = int(input("Enter the number:- "))
+if num>=0:
+    if num == 0:
+        print("Number is zero.")
+    else:
+        print(f"{num} Positive number")
+else:
+    print(f"{num} Negative number")

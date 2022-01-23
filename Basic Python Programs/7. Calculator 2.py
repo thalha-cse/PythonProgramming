bade373600306dd853bc36bd786d4e3557ff9375
MOHAMMED THALHA J <96979827+Mohammed-Thalha@users.noreply.github.com>
2022-01-23 07:20:52 -0800

    Create 7. Calculator 2.py

---
+++ Basic Python Programs/7. Calculator 2.py
@@ -0,0 +1,12 @@
+num1 = int(input("Enter the number:- "))
+num2 = int(input("Enter the number:- "))
+
+sum = float(num1) + float(num2)
+dif = float(num1) + float(num2)
+div = float(num1) + float(num2)
+mul = float(num1) + float(num2)
+
+print("Addition : {0}".format(sum))
+print("subtractiion: {0}".format(dif))
+print("Division : {0}".format(div))
+print("Multiplication : {0}".format(mul))

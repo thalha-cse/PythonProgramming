1b760e88f9b2ffa468cc7d337a47d2cbd21a62b9
MOHAMMED THALHA J <96979827+Mohammed-Thalha@users.noreply.github.com>
2022-01-23 07:23:26 -0800

    Delete 10. Area of Triangle.py

--- Basic Python Programs/10. Area of Triangle.py
+++
@@ -1,6 +0,0 @@
-x = int(input("Enter the first side:- "))
-y = int(input("Enter the second side:- "))
-z = int(input("Enter the thrid side:- "))
-
-s = (x + y + z) / 2
-print("The area of Triangle is : {0}".format(s))

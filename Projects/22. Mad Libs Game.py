7f24b4983837dc3df72783ef6d6145ca85cde3ec
MOHAMMED THALHA J <96979827+Mohammed-Thalha@users.noreply.github.com>
2022-01-29 01:53:35 -0800

    Create 22. Mad Libs Game.py

---
+++ Projects/22. Mad Libs Game.py
@@ -0,0 +1,11 @@
+# MAD LIBS is one of the funniest word games in the world. Some would even call it one of the greatest puns ever.
+# Creating a Mad Libs game is one of the simplest tasks for beginners who want to enter the world of software development.
+
+
+
+color = input("Enter the color : ")
+plural_noun = input("Enter the Plural Noun : ")
+celebrity = input("Enter the name of celebrity : ")
+print("The roses are", color)
+print(plural_noun, "is a noun")
+print("I'm the big fan of", celebrity)
